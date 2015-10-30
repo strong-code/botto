d14a7433bd3037ec53848e21af6cc2241dde1ca7
clindsay107 <clindsay107@gmail.com>
2015-10-30 13:18:48 -0700

    Move config into its own module. Add example config and debugging output

--- botto.js
+++ botto.js
@@ -1,15 +1,16 @@
-// Create the configuration
-var config = {
-	channels: ["#bottotest"],
-	server: "irc.rizon.net",
-	botName: "botto"
-};
+var config = require("./config").config
 
 var irc = require("irc");
 
 var bot = new irc.Client(config.server, config.botName, {
   channels: config.channels
 });
+
+if (config.debug) {
+	bot.addListener("message", function(from, to, msg) {
+		console.log("[" + to + "] " + from + ": " + msg);
+	});
+}
 
 bot.addListener("join", function(chan, who) {
   bot.say(chan, "welcome back " + who);
---
+++ example.config.js
@@ -0,0 +1,9 @@
+var config = {
+	channels: ["#channel"],
+	server: "server.address.here",
+	botName: "ABotName",
+  password: "BotPassword",
+  debug: false
+};
+
+exports.config = config
