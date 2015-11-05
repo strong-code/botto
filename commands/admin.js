2fc12f968e6b89ecd8d90a35028603cefd81993c
clindsay107 <clindsay107@gmail.com>
2015-11-04 16:30:09 -0800

    Simplify admin module for now. Add ping module. Maybe show latency with an arg passed at some point?

--- commands/admin.js
+++ commands/admin.js
@@ -1,25 +1,16 @@
 var admins = require("../config.js").admin;
 
 /*
- * All commands should export a single function which acts as a routing layer
- * for calling the appropriate, non-exported function.
- */
-module.exports = function (opts) {
-  if (opts.args.length == 0 || opts.to[0] != '#') {
-    return
-  } else {
-    return isAdmin(opts.args[0], opts.to);
-  }
-};
-
-/*
  * Determine if a supplied user (by nickname) is an admin for the bot as defined
  * in the config file.
  *
  * !admin user123
  */
-function isAdmin(nick, channel) {
+module.exports = function (opts) {
+  var channel = opts.to;
+  var nick = opts.args[0];
+
   if (admins[channel]) {
     return admins[channel].indexOf(nick) > -1;
   }
-}
+};
---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+/*
+ * Respond to a user with a PONG message. Helpful for when a user needs to be
+ * highlighted for testing or to check connection latency.
+ */
+
+module.exports = function(opts) {
+  return opts.from + ": PONG";
+}
