f6b2f5852c69e84d08ea1208f2891d274eaa1431
clindsay107 <clindsay107@gmail.com>
2015-11-10 15:53:08 -0800

    Add admin list command

--- commands/admin.js
+++ commands/admin.js
@@ -10,7 +10,19 @@ module.exports = function (opts, respond) {
   var channel = opts.to;
   var nick = opts.args[0];
 
-  if (admins[channel]) {
+  if (nick == 'list') {
+    respond(listAdmins(channel));
+  } else if (admins[channel]) {
     respond(admins[channel].indexOf(nick) > -1);
   }
+
 };
+
+// List all admins for a specified channel
+function listAdmins(channel) {
+  if (admins[channel]) {
+    return "Admins for " + channel + ": " + admins[channel].join(', ');
+  } else {
+    return "No administrators specified for " + channel;
+  }
+}
