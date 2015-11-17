50d215002d5aa8373e9f1c2787529f762977cfd4
clindsay107 <clindsay107@gmail.com>
2015-11-16 16:28:24 -0800

    Add help documentation and on-the-fly upload command

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+var exec = require('child_process').exec;
+
+module.exports = {
+
+  call: function(opts, respond) {
+    exec("cat ./help.txt | curl -F 'sprunge=<-' http://sprunge.us", function(error, stdout, stderr) {
+      if (error) {
+        console.error(error);
+      } else {
+        respond("Help is on the way: " + stdout);
+      }
+    });
+  }
+
+};
