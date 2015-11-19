8e4cf388ba72b03a3cc6d99424ac14d5d3ebb43e
clindsay107 <clindsay107@gmail.com>
2015-11-19 11:43:56 -0800

    Flesh out IRC commands core module

--- core/irc.js
+++ core/irc.js
@@ -15,6 +15,18 @@ module.exports = {
         case 'say':
           module.exports.say(bot, opts);
           break;
+        case 'action':
+          module.exports.action(bot, opts);
+          break;
+        case 'notice':
+          module.exports.notice(bot, opts);
+          break;
+        case 'ctcp':
+          module.exports.ctcp(bot, opts);
+          break;
+        case 'cycle':
+          module.exports.cycle(bot, opts);
+          break;
       }
     }
   },
@@ -41,6 +53,39 @@ module.exports = {
       bot.say(opts.args[1], opts.args.slice(2).join(' '));
     } else {
       bot.say(opts.to, "Not enough parameters specified. Usage is !irc say <receiver> <message>");
+    }
+  },
+
+  action: function(bot, opts) {
+    if (opts.args[1] && opts.args[2]) {
+      bot.action(opts.args[1], opts.args.slice(2).join(' '));
+    } else {
+      bot.say(opts.to, "Not enough parameters specified. Usage is !irc action <receiver> <message>");
+    }
+  },
+
+  notice: function(bot, opts) {
+    if (opts.args[1] && opts.args[2]) {
+      bot.notice(opts.args[1], opts.args.slice(2).join(' '));
+    } else {
+      bot.say(opts.to, "Not enough parameters specified. Usage is !irc notice <receiver> <message>");
+    }
+  },
+
+  ctcp: function(bot, opts) {
+    if (opts.args[1] && opts.args[2]) {
+      bot.ctcp(opts.args[1], opts.args.slice(2).join(' '));
+    } else {
+      bot.say(opts.to, "Not enough parameters specified. Usage is !irc ctcp <receiver> <message>");
+    }
+  },
+
+  cycle: function(bot, opts) {
+    if (opts.args[1]) {
+      bot.part(opts.args[1]);
+      bot.join(opts.args[1]);
+    } else {
+      bot.say(opts.to, "No channel specified");
     }
   }
 
