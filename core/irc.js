c2c3a0c0aefb5d266064748c54236de3bb5c2f1b
clindsay107 <clindsay107@gmail.com>
2015-11-23 22:45:42 -0800

    Clean up IRC module

--- core/irc.js
+++ core/irc.js
@@ -32,58 +32,77 @@ module.exports = {
   },
 
   join: function(bot, opts) {
-    if (opts.args[1]) {
-      bot.join(opts.args[1])
+    var chan = opts.args[1];
+
+    if (chan) {
+      bot.join(chan)
     } else {
       bot.say(opts.to, "No channel specified");
     }
   },
 
   part: function(bot, opts) {
-    if (opts.args[1]) {
+    var chan = opts.args[1];
+
+    if (chan) {
       var msg = opts.args.length > 2 ? opts.args.slice(2).join(' ') : "cya nerds";
-      bot.part(opts.args[1], msg);
+      bot.part(chan, msg);
     } else {
       bot.say(opts.to, "No channel specified");
     }
   },
 
   say: function(bot, opts) {
-    if (opts.args[1] && opts.args[2]) {
-      bot.say(opts.args[1], opts.args.slice(2).join(' '));
+    var receiver = opts.args[1];
+    var message = opts.args[2],join(' ');
+
+    if (receiver && message) {
+      bot.say(receiver, message);
     } else {
       bot.say(opts.to, "Not enough parameters specified. Usage is !irc say <receiver> <message>");
     }
   },
 
   action: function(bot, opts) {
-    if (opts.args[1] && opts.args[2]) {
-      bot.action(opts.args[1], opts.args.slice(2).join(' '));
+    var receiver = opts.args[1];
+    var action = opts.args[2],join(' ');
+
+    if (receiver && action) {
+      bot.action(receiver, action);
     } else {
       bot.say(opts.to, "Not enough parameters specified. Usage is !irc action <receiver> <message>");
     }
   },
 
   notice: function(bot, opts) {
-    if (opts.args[1] && opts.args[2]) {
-      bot.notice(opts.args[1], opts.args.slice(2).join(' '));
+    var receiver = opts.args[1];
+    var message = opts.args.slice(2).join(' ');
+
+    if (receiver && message) {
+      bot.notice(receiver, message);
     } else {
       bot.say(opts.to, "Not enough parameters specified. Usage is !irc notice <receiver> <message>");
     }
   },
 
   ctcp: function(bot, opts) {
-    if (opts.args[1] && opts.args[2]) {
-      bot.ctcp(opts.args[1], opts.args[2], opts.args.slice(3).join(' '));
+    var receiver = opts.args[1];
+    var type = opts.args[2];
+    var text = opts.args.slice(3).join(' ');
+
+    if (receiver && type) {
+      bot.ctcp(receiver, type, text);
     } else {
       bot.say(opts.to, "Not enough parameters specified. Usage is !irc ctcp <receiver> <message>");
     }
   },
 
   cycle: function(bot, opts) {
-    if (opts.args[1]) {
-      bot.part(opts.args[1]);
-      bot.join(opts.args[1]);
+    var chan = opts.args[1];
+
+    if (chan) {
+      bot.part(chan);
+      bot.join(chan);
     } else {
       bot.say(opts.to, "No channel specified");
     }
