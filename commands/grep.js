560e95d39cd18f3d99af2edfe575661c3786d549
strong-code <colin@strongco.de>
2016-05-13 11:49:49 -0700

    just put it all in 1 command and pipe to termbin upload

--- commands/grep.js
+++ commands/grep.js
@@ -16,23 +16,13 @@ module.exports = {
 
   grep: function (query, channel, respond) {
     var path = logPath + "/" + channel;
-    var cmd = "grep -R \"" + query + "\" " + path;
-    return exec(cmd, function (err, stdout, stderr) {
-      if (err) {
-        return respond('[ERROR]: ' + err);
-      }
-      return module.exports.upload(stdout, respond);
-    });
-  },
-
-  upload: function (results, respond) {
-    var cmd = "echo \"" + results + "\" | nc termbin.com 9999";
+    var cmd = "grep -R \"" + query + "\" " + path + " | nc termbin.com 9999";
     return exec(cmd, function (err, stdout, stderr) {
       if (err) {
         return respond('[ERROR]: ' + err);
       }
       return respond('Search results: ' + stdout);
     });
-  }
+  },
 
 };
