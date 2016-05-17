62198a26ed1be412f330ac36ef5c19c3c064f5d1
strong-code <colin@strongco.de>
2016-05-17 11:56:03 -0700

    add user nick when saving replies

--- commands/reply.js
+++ commands/reply.js
@@ -6,13 +6,13 @@ module.exports = {
 
   call: function (opts, respond) {
     if (opts.args[0] === 'add') {
-      return module.exports.addReply(_.drop(opts.args), respond);
+      return module.exports.addReply(_.drop(opts.args), opts.from, respond);
     } else if (opts.args[0] === 'remove' && admin.isAdmin(opts.from, opts.to)) {
       // return module.exports.removeReply(_.drop(opts.args), respond);
     }
   },
 
-  addReply: function (opts, respond) {
+  addReply: function (opts, nick, respond) {
     var args = _.join(opts.args, ' ');
     var data = _.split(args, ' <reply> ');
     var trigger = data[0];
@@ -20,7 +20,7 @@ module.exports = {
     return db.executeQuery({
       text: 'INSERT INTO replies (added_by, trigger, reply, enabled, date_added) ' +
         'VALUES ($1, $2, $3, $4, $5)',
-      values: [opts.from, trigger, reply, true, new Date().toISOString()]
+      values: [nick, trigger, reply, true, new Date().toISOString()]
     }, function () {
       return respond('Trigger added');
     });
