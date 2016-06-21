9a6f5f4ef8aa7b690a9147d4ce1f55de93638fea
strong-code <clindsay107@gmail.com>
2016-06-21 13:33:54 -0700

    up the timeout

--- commands/ether.js
+++ commands/ether.js
@@ -54,7 +54,7 @@ var url = 'https://poloniex.com/public?command=returnTicker';
 
 var options = {
   follow: 3,
-  open_timeout: 5000,
+  open_timeout: 20000,
   headers: {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1677.0 Safari/537.36"
   }
