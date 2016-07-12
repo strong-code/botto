dd9b0396c45510ee17b10cff623316f1109210c9
strong-code <colin@strongco.de>
2016-07-12 16:29:04 -0700

    syntax error

--- core/health.js
+++ core/health.js
@@ -14,7 +14,7 @@ module.exports  = {
     // Only available in node 6.1+, which breaks pg module...
     //const cpu    = process.cpuUsage();
 
-    return `Uptime: ${uptime)} | Memory [rss] ${Math.round(Number(memory.rss/1024) * 10) / 10} Kb [heapTotal] ${Math.round(Number(memory.heapTotal/1024) * 10) / 10} Kb [heapUsed] ${Math.round(Number(memory.heapUsed/1024) * 10) / 10} Kb`;
+    return `Uptime: ${uptime} | Memory [rss] ${Math.round(Number(memory.rss/1024) * 10) / 10} Kb [heapTotal] ${Math.round(Number(memory.heapTotal/1024) * 10) / 10} Kb [heapUsed] ${Math.round(Number(memory.heapUsed/1024) * 10) / 10} Kb`;
   }
 
 };
