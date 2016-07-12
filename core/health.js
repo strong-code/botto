56b48982526bda19ae44619afa4e014a8de916fe
strong-code <colin@strongco.de>
2016-07-12 16:27:25 -0700

    fix human readable time

--- core/health.js
+++ core/health.js
@@ -9,12 +9,12 @@ module.exports  = {
   },
 
   getHealth: function() {
-    const uptime = process.uptime();
+    const uptime = moment.duration(process.uptime()).humanize();
     const memory = process.memoryUsage();
     // Only available in node 6.1+, which breaks pg module...
     //const cpu    = process.cpuUsage();
 
-    return `Uptime: ${moment.duration(uptime).humanize()} seconds | Memory [rss] ${Math.round(Number(memory.rss/1024) * 10) / 10} Kb [heapTotal] ${Math.round(Number(memory.heapTotal/1024) * 10) / 10} Kb [heapUsed] ${Math.round(Number(memory.heapUsed/1024) * 10) / 10} Kb`;
+    return `Uptime: ${uptime)} | Memory [rss] ${Math.round(Number(memory.rss/1024) * 10) / 10} Kb [heapTotal] ${Math.round(Number(memory.heapTotal/1024) * 10) / 10} Kb [heapUsed] ${Math.round(Number(memory.heapUsed/1024) * 10) / 10} Kb`;
   }
 
 };
