7225a93e1a8152c19bdb3ec88b2c2b14dbcd22a8
strong-code <clindsay107@gmail.com>
2016-07-15 14:34:46 -0700

    fix start script with forever monitor

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-forever stop && git checkout master && git pull && npm install && screen -d -m -S botto node start.js
+forever stopall && git checkout master && git pull && npm install --no-optional && screen -d -m -S botto node start.js
