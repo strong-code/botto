298a6d0316c5866622bb8e16f182cd55c4d316cc
clindsay107 <clindsay107@gmail.com>
2016-07-27 18:24:34 -0700

    add botto responses on name call. need to create table and add reply trigger

--- observers/botto.js
+++ observers/botto.js
@@ -1,20 +1,31 @@
 const fs = require('fs');
+const _  = require('lodash');
+const db = require('../core/_db.js');
 
 module.exports = {
 
   call: function(opts, respond) {
-    if (opts.text === 'botto') {
-      return respond(me);
+    const text = opts.text.split(' ');
+    if (_.includes(text, 'botto') && text[0] !== '!bottoreply') {
+      return getResponse(respond);
     }
+  },
+
+  getResponse: function(respond) {
+    return db.executeQuery('SELECT * FROM botto_replies ORDER BY RANDOM() LIMIT 1', function(result) {
+      if (result.rows && result.rows[0]) {
+        return respond(result.rows[0]['message']);
+      }
+    });
   }
 
 };
 
-const me = `┈╭━━━━━━━━━━━╮┈
-┈┃╭━━━╮┊╭━━━╮┃┈
-╭┫┃┈🦄┈┃┊┃┈🦄┈┃┣
-┃┃╰━━━╯┊╰━━━╯┃┃
-╰┫╭━╮╰━━━╯╭━╮┣╯
-┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈
-┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈
-┈╰━━━━━━━━━━━╯┈`
+// const me = `┈╭━━━━━━━━━━━╮┈
+// ┈┃╭━━━╮┊╭━━━╮┃┈
+// ╭┫┃┈🦄┈┃┊┃┈🦄┈┃┣
+// ┃┃╰━━━╯┊╰━━━╯┃┃
+// ╰┫╭━╮╰━━━╯╭━╮┣╯
+// ┈┃┃┣┳┳┳┳┳┳┳┫┃┃┈
+// ┈┃┃╰┻┻┻┻┻┻┻╯┃┃┈
+// ┈╰━━━━━━━━━━━╯┈`
