fe8acc0f8f697c95f83af95e3fb221285364780a
strong-code <clindsay107@gmail.com>
2016-08-19 10:37:56 -0700

    fix markov parsing and analysis

--- observers/markov.js
+++ observers/markov.js
@@ -1,5 +1,3 @@
-const walk       = require('walk');
-const linereader = require('line-reader')
 const _          = require('lodash');
 let isWarmed     = false;
 let cache        = {};
@@ -7,37 +5,36 @@ let cache        = {};
 module.exports = {
 
   call: function (opts, respond) {
-
+    const text = opts.text.split(' ');    
+    if (_.includes(text, 'botto')) {
+      //do something
+    }  
   },
 
-  warmCache = function () {
+  warmCache: function () {
     cache = {};
-    const walker = walk.walk('./irclogs/Rizon/2016', { followLinks: false });
-    let files = [];
+    const execFile = require('child_process').execFile;
 
-    walker.on('file', (root, stat, next) => {
-      if (stat.type === 'file' && _.endsWith(stat.name, '.log')) {
-        files.push(root + '/' + stat.name);
-        next();
-      }
-    });
-
-    walker.on('end', () => {
-      readLines(files);
+    execFile('find', ['/root/irclogs/Rizon/#lifting/2016'], (err, stdout, stderr) => {
+      let files = stdout.split('\n');
+      files = _.filter(files, (f) => {
+        return _.endsWith(f, '.log');
+      });
+      return module.exports.readLines(files)
     });
   },
 
   readLines: function (files) {
     const linereader = require('readline').createInterface({
-      input: require('fs').createReadStream(files.shift());
+      input: require('fs').createReadStream(files.shift())
     });
 
-    linreader.on('line', (line) => {
+    linereader.on('line', (line) => {
       let parts = line.split(' ');
       if (parts[1].slice(-1) === ':') {
         let words = parts.slice(2);
-
-        for (i = 0; i < words.length; i++) {
+        
+        for (var i = 0; i < words.length; i++) {
           let curWord  = words[i];
           let nextWord = words[i+1];
 
@@ -52,8 +49,11 @@ module.exports = {
 
     linereader.on('close', () => {
       if (!files.length || files.length === 0) {
+        //cache = _.filter(cache, (obj) => {
+        //  return obj.length !== 0;
+        //});
+        console.log(cache['I'])
         console.log('Cache has been warmed')
-        console.log(cache)
         isWarmed = true;
         return;
       }
