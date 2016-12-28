be9379228a04f72461e531072497dc9b06d62081
strong-code <clindsay107@gmail.com>
2016-12-27 16:51:28 -0800

    add unmount list

--- commands/unmount.js
+++ commands/unmount.js
@@ -1,15 +1,16 @@
 const _ = require('lodash');
-const unmount = require('./_commandHandler.js').unmount;
-const mount = require('./_commandHandler.js').mount;
+const commands = require('./_commandHandler.js')
 
 module.exports = {
 
   call: function(opts, respond) {
     if (opts.args[0] === 'del') {
-      mount(opts.to, opts.args[1]);
+      commands.mount(opts.to, opts.args[1]);
       return respond(opts.args[1] + ' no longer unmounted in ' + opts.to);
+    } else if (opts.args[0] === 'list') {
+      return respond('Currently unmounted triggers in ' + opts.to + ': ' + _.join(commands.unmounted[opts.to], ', '));
     } else {
-      unmount(opts.to, opts.args[0]);      
+      commands.unmount(opts.to, opts.args[0]);      
       return respond(opts.args[0] + ' unmounted in ' + opts.to);
     }
   }
