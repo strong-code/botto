fbae42e95f3879469bc459dd6767aee716092e8d
strong-code <colin@strongco.de>
2017-06-23 16:59:49 -0700

    add who said that

--- observers/reply.js
+++ observers/reply.js
@@ -4,15 +4,23 @@ var _  = require('lodash');
 module.exports = {
 
   call: function (opts, respond) {
-    db.executeQuery({
-      text: 'SELECT * FROM replies WHERE trigger=$1 ORDER BY RANDOM() LIMIT 1',
-      values: [opts.text]
-    }, function (result) {
-      if (result.rows[0] && result.rows[0]['enabled'] === true) {
-        module.exports.last = result.rows[0];
-        return respond(result.rows[0]['reply']);
+    if (opts.text === 'who said that') {
+      if (module.exports.last) {
+        return respond('This shitpost brought to you by ' + module.exports.last['added_by']);
+      } else {
+        return respond('who said what?');
       }
-    });
+    } else {
+      db.executeQuery({
+        text: 'SELECT * FROM replies WHERE trigger=$1 ORDER BY RANDOM() LIMIT 1',
+        values: [opts.text]
+      }, function (result) {
+        if (result.rows[0] && result.rows[0]['enabled'] === true) {
+          module.exports.last = result.rows[0];
+          return respond(result.rows[0]['reply']);
+        }
+      });
+    }
   },
 
   last: undefined
