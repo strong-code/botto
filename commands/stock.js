dd5060118c704f197d34f8386da01669787ccd08
clindsay107 <clindsay107@gmail.com>
2018-01-30 14:03:32 -0800

    remove unused function call

--- commands/stock.js
+++ commands/stock.js
@@ -33,5 +33,3 @@ const httpOpts = {
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1677.0 Safari/537.36"
   }
 };
-
-module.exports.stockInfo('MSFT');
