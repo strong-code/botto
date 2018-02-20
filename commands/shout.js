ff140aa23a8126345daab3f6e013ac35e83955c7
clindsay107 <clindsay107@gmail.com>
2018-02-19 21:31:47 -0800

    shout: add ability to delete by nick

--- commands/shout.js
+++ commands/shout.js
@@ -1,8 +1,12 @@
-var db = require('../core/_db.js');
+const db = require('../core/_db.js')
+const admin = require('../core/admin.js')
 
 module.exports = {
 
   call: function(opts, respond) {
+    if (opts.args[0] == 'delete' && admin.isAdmin(opts.from, opts.to)) {
+      return module.exports.deleteShoutsForNick(opts.args[1], respond)
+    }
     if (opts.args[0].length) {
       return module.exports.getShoutForNick(opts.args[0], respond);
     } else {
@@ -28,5 +32,16 @@ module.exports = {
         return respond(result.rows[0]['message']);
       }
     });
+  },
+
+  deleteShoutsForNick(nick, respond) {
+    return db.executeQuery({
+      text: "DELETE FROM shouts WHERE NICK = $1",
+      values: [nick]
+    }, (result) => {
+      if (result.rows && result.rows[0]) {
+        return respond(result.rows[0]['message'])
+      }
+    })
   }
 }
