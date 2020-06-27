1d5c376096ec6d9fdd7bee0d61af0927e0203554
strong-code <colin@strongco.de>
2020-06-27 00:30:13 -0700

    logs: allow user logs lookup and condense code

--- commands/logs.js
+++ commands/logs.js
@@ -1,15 +1,21 @@
 const { exec } = require('child_process')
 const needle = require('needle')
+const _ = require('lodash')
 const logs = require('../config.js').logs
 
+// TODO: the respond() cb is being passed around a lot
+// so that it can be called early with error messages. Would
+// be better to throw exceptions on error and catch in call()
 module.exports = {
 
   call: function(opts, respond) {
-    const lines = opts.args[0] || 25
-    
-    module.exports.getLocalLogs(lines, (out) => {
-      respond(out)
-    })
+    // if first arg is NaN e.g. !logs foo
+    // get user logs, else use args[0]
+    if (isNaN(parseInt(opts.args[0]))) { 
+      module.exports.getUserLogs(opts.args, (out) => respond(out))
+    } else {
+      module.exports.getLocalLogs(opts.args[0], (out) => respond(out))
+    }
   },
 
   upload: function(data, cb) {
@@ -25,24 +31,33 @@ module.exports = {
     })
   },
 
+  // Always take first filter, then iterate over remainder
+  // and build grep chain
+  getUserLogs: function(filters, cb) {
+    let cmd = `grep '${filters[0]}' ${logs.path}`
+    _.forEach(_.tail(filters), (filter) => {
+       cmd += ` | grep '${filter}'`
+    })
+    module.exports.runCommand(cmd, cb)
+  },
+
   getLocalLogs: function(lines, cb) {
+    lines = lines || 25
     const cmd = `tail -n ${lines} ${logs.path}`
-    exec(cmd, (e, out, err) => {
+    module.exports.runCommand(cmd, cb)
+  },
+
+  runCommand: function(cmd, cb) {
+    return exec(cmd, (e, out, err) => {
       if (e) {
         console.log(e.message)
-        return cb('Ironically, something went wrong and you need to check logs')
+        return cb('Ironically, something went wrong and you need to check logs', true)
       }
       if (err) {
         console.log('Error retrieving local logs: ' + err)
-        return cb(err)
+        return cb(err, true)
       }
-      if (lines < 3) {
-        return cb(out)
-      } else {
-        return module.exports.upload(out, (url) => {
-          return cb(url)
-        })
-      }
+        return module.exports.upload(out, (url) => cb(url))
     })
   }
 }
