296e2b28c6470606faf265bb890d2168c5b121a6
strong-code <clindsay107@gmail.com>
2020-06-30 23:26:26 -0700

    git: automatically hotswap module when git updates them

--- core/git.js
+++ core/git.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec
-/*
- * IRC-triggerable git commands for pulling in new changes
- */
+const _ = require('lodash')
+const needle = require('needle')
+const LOGS_API = require('../config.js').logs.api
 
 module.exports = {
 
@@ -22,7 +22,37 @@ module.exports = {
       if (err) {
         return bot.say(opts.to, err.message + "; Check logs for more info");
       }
-      return bot.say(opts.to, stdout);
+
+      // split on newline, drop first 2 lines and last line
+      let updated = stdout.split(/\r?\n/).slice(2).slice(0,-1)
+
+      // if not undefined and has length (e.g. there was at least 1 file update)
+      if (updated.length > 0) {
+        // Parse out relative file paths
+        updated = _.map(updated, (entry) => entry.split(' | ')[0].trim())
+        // Only the .js files
+        const modules = _.filter(updated, (entry) => entry.substr(-2) === 'js')
+
+        _.forEach(modules, (f) => {
+          delete require.cache[require.resolve('../'+f)] 
+        })
+
+        let updateMsg = `${modules.length} modules updated [${updated.length} total}. Full output: `
+
+        needle.post(LOGS_API, `text=${stdout}`, {}, (err, res) => {
+          if (err) {
+            return bot.say(opts.to, err.message)
+          }
+          if (!res.body.path) {
+            return bot.say(opts.to, `Error while uploading output to API: ${logs.api}`)
+          }
+          updateMsg += res.body.path
+          bot.say(opts.to, updateMsg)
+        })
+      } else {
+        // Normal stdout from `git pull`
+        return bot.say(opts.to, stdout)
+      }
     });
   },
 
