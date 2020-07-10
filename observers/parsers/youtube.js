b88645562c940eddcc7d79ef99ef32e563f9e6ff
strong-code <clindsay107@gmail.com>
2020-07-09 17:10:18 -0700

    youtube: clean up code

--- observers/parsers/youtube.js
+++ observers/parsers/youtube.js
@@ -39,28 +39,24 @@ module.exports = {
 
     // If the string contains hours parse it and remove it from our duration string
     if (duration.indexOf('H') > -1) {
-      hours_split = duration.split('H');
-      hours       = parseInt(hours_split[0]);
+      let hours = parseInt(duration.split('H')[0])
       timeStr += `${hours}h `
-      duration    = hours_split[1];
+      duration = hours_split[1]
     }
 
     // If the string contains minutes parse it and remove it from our duration string
     if (duration.indexOf('M') > -1) {
-      minutes_split = duration.split('M');
-      minutes       = parseInt(minutes_split[0]);
+      let minutes = parseInt(duration.split('M')[0])
       timeStr += `${minutes}m `
-      duration      = minutes_split[1];
+      duration = minutes_split[1]
     }
 
     // If the string contains seconds parse it and remove it from our duration string
     if (duration.indexOf('S') > -1) {
-      seconds_split = duration.split('S');
+      let seconds = parseInt(duration.split('S')[0])
       timeStr += `${seconds}s`
-      seconds       = parseInt(seconds_split[0]);
     }
 
-    // Math the values to return seconds
     return timeStr + ''
   }
 
