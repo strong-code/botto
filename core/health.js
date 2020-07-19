a5cfbac7bddb12ecef7ca319ecc89cc18b78c721
strong-code <colin@strongco.de>
2020-07-18 19:29:05 -0700

    health: remove colors, update syntax

--- core/health.js
+++ core/health.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-const colors = require('irc').colors;
 const moment = require('moment');
 
 module.exports  = {
@@ -10,29 +9,23 @@ module.exports  = {
   },
 
   getHealth: function() {
-    const uptime  = colors.wrap('dark_green', moment.duration(process.uptime(), 'seconds').humanize());
-    const memory  = module.exports.getMemory();
-    const version = colors.wrap('dark_green', process.version);
-    const modules = colors.wrap('dark_green', module.exports.getModules());
+    const uptime  = moment.duration(process.uptime(), 'seconds').humanize()
+    const memory  = module.exports.getMemory()
+    const version = process.version
+    const modules = module.exports.getModules()
 
-    return `Uptime ${uptime} | Memory ${memory}Mb | Modules ${modules} | Node ${version}`;
+    return `Uptime ${uptime} | Memory ${memory}Mb | Modules ${modules} | Node ${version}`
   },
 
   getMemory: function() {
-    const memory = process.memoryUsage().rss / 1024576;
-    let c = 'dark_green';
-
-    if (memory > 35) {
-      c = 'dark_red';
-    }
-    
-    return colors.wrap(c, Math.round(Number(memory)));
+    const memory = process.memoryUsage().rss / 1024576
+    return Math.round(Number(memory))
   },
 
   getModules: function() {
     return _.filter(require.cache, (v, k) => {
-      return !_.includes(k, 'node_modules');
-    }).length;
+      return !_.includes(k, 'node_modules')
+    }).length
   }
 
 };
