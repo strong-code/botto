6aa08e94b6c608f0d0666a0c2c71c3e7697642fc
strong-code <colin@strongco.de>
2020-07-18 23:39:20 -0700

    emoji: add aliasing and simplify

--- observers/emoji.js
+++ observers/emoji.js
@@ -6,10 +6,25 @@ module.exports = {
   call: function(opts, respond) {
     const match = opts.text.match(regex)
 
-    if (match && emoji.hasEmoji(match[0])) {
-      const e = emoji.get(match[0])
-      respond(e)
+    if (match) {
+      module.exports.getEmoji(match[0], (e) => respond(e))
     }
+  },
+
+  getEmoji: function(name, cb) {
+    if (module.exports.alias[name]) {
+      name = module.exports.alias[name]
+    }
+
+    if (emoji.hasEmoji(name)) {
+      cb(emoji.get(name))
+    }
+  },
+
+  // TODO: move this into a separate file if it grows too big
+  // or if it needs to be shared elsewhere
+  alias: {
+    ':thinking:': 'thinking_face:'
   }
 
 }
