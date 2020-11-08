102b8531300ed35583d94c2ed0d7ced18fd6c6df
strong-code <colin@strongco.de>
2020-11-07 19:27:24 -0800

    remind: use correct regex fn. Log all errors

--- commands/remind.js
+++ commands/remind.js
@@ -5,7 +5,7 @@ module.exports = {
   
   call: function (opts, respond) {
     try {
-      let [, count, unit, reminder] = regex.match(_.join(opts.args, ' '))
+      let [, count, unit, reminder] = regex.exec(_.join(opts.args, ' '))
       count = module.exports.convertCount(count, unit)
 
       setTimeout(() => {
@@ -14,11 +14,11 @@ module.exports = {
 
       respond(`I will remind you in ${count} ${unit}`)
     } catch (e) {
+      console.log(e)
       // TypeError thrown when regex cant match array destructuring
       if (e instanceof TypeError) {
         return respond('Usage is !remind <count> <unit> <message>. <unit> can be seconds, minutes, hours or days.')
       } else {
-        console.log(e)
         return respond('Something went wrong, please check !logs')
       }
     }
