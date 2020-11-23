27b159b82223ec02b7b4160042f0e3086131a3a2
strong-code <colin@strongco.de>
2020-11-23 12:04:14 -0800

    parsers/imdb: use a real user agent for imdb calls. Defer to general parser on error

--- observers/parsers/imdb.js
+++ observers/parsers/imdb.js
@@ -9,13 +9,17 @@ module.exports = {
     const movieId = url.pathname.split('/')[2]
     const apiUrl = `http://www.omdbapi.com/?i=${movieId}&apikey=${config.omdb.apiKey}`
 
+    // overwrite bot user agent so we don't trigger CloudFlare
+    config.options.headers = { 
+      'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1677.0 Safari/537.36"
+    }
+
     needle.get(apiUrl, config.options, (err, res) => {
       if (err) {
         console.log(err)
-        return respond('Unable to parse details for IMDB ID ' + movieId)
+        return cb(false)
       }
       const m = res.body
-      console.log(m)
       const info = `[IMDB] "${m.Title}" (${m.Year}) | ${m.Rated} | ${m.Runtime} | ${m.Genre} |`+
         ` ★ ${m.imdbRating} | Directed by ${m.Director} | ${m.Plot}`
 
