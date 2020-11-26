bd35a2884bc1ec799e51e7eca08be183380fef64
strong-code <colin@strongco.de>
2020-11-26 14:20:34 -0800

    parsers/reddit: show parser name in response

--- observers/parsers/reddit.js
+++ observers/parsers/reddit.js
@@ -18,7 +18,7 @@ module.exports = {
     const t = await r.getSubmission(thread).fetch()
     const date = new Date(t.created*1000).toLocaleString().split(' ')[0].slice(0,-1)
 
-    return `${t.subreddit_name_prefixed}: "${t.title}" posted by u/${t.author.name} on ${date} `+
+    return `[Reddit] ${t.subreddit_name_prefixed}: "${t.title}" posted by u/${t.author.name} on ${date} `+
       `| ${t.comments.length} comments | ${t.ups}↑ - ${t.downs}↓` 
   }
 
