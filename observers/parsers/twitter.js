65242cc413087862998e5b7c38bf099f5eed663a
strong-code <colin@strongco.de>
2020-12-22 19:49:38 -0800

    parsers/twitter: use twit and then http scrape as fallback

--- observers/parsers/twitter.js
+++ observers/parsers/twitter.js
@@ -1,5 +1,7 @@
 const needle = require('needle')
 const config = require('../../config').url
+const Twit = require('twit')
+const T = new Twit(require('../../config').twitter)
 const cheerio = require('cheerio')
 
 module.exports = {
@@ -11,11 +13,23 @@ module.exports = {
       throw Error('Not a tweet page, using default parser')
     }
 
+    try {
+      const tweetId = url.path.split('/')[3]
+      const response = await T.get('statuses/show/:id', { id: tweetId, tweet_mode: 'extended' })
+      const data = response.data
+      return `[Twitter] @${data.user.screen_name}: ${data.full_text.strip()}`
+    } catch (e) {
+      console.log(`Error retrieving tweet with Twit library, trying with HTTP request. \n ${e}`)
+      const info = await module.exports.getHttp(url)
+      return info
+    }
+  },
+
+  getHttp: async function(url) {
     const res = await needle('get', url.href, config.options)
     const $ = cheerio.load(res.body)
     const username = '@' + url.path.split('/')[1]
     const description = $('meta[property="og:description"]').attr('content').replace(/\r?\n|\r/g, " ")
-
     return `[Twitter] ${username}: ${description}`
   }
 
