cebf1100bd2933ced104bc883def142c8514fec0
strong-code <colin@strongco.de>
2021-07-09 14:34:18 -0700

    Move reply observer to es6 classes. Last major class for this pr

--- observers/reply.js
+++ observers/reply.js
@@ -1,32 +1,28 @@
-var db = require('../util/db.js')
-var _  = require('lodash');
+const db = require('../util/db.js')
+const Observer = require('./observer.js')
 
-module.exports = {
+module.exports = class Reply extends Observer {
 
-  call: function (opts, respond) {
+  constructor() {
+    const regex = new RegExp(/.*/)
+    super('reply', regex)
+  }
+
+  #lastReply = undefined
+
+  async call(opts, respond) {
     if (opts.text === 'who added that') {
-      return module.exports.who(respond);
+      return respond(this.#lastReply ? this.#lastReply.added_by : 'who added what?')
     } else {
-      db.executeQuery({
-        text: 'SELECT * FROM replies WHERE trigger=$1 ORDER BY RANDOM() LIMIT 1',
-        values: [opts.text]
-      }, function (result) {
-        if (result.rows[0] && result.rows[0]['enabled'] === true) {
-          module.exports.last = result.rows[0];
-          return respond(result.rows[0]['reply']);
-        }
-      });
+      const row = await db.oneOrNone(
+        'SELECT * FROM replies WHERE trigger = $1 AND enabled = true ORDER BY RANDOM() LIMIT 1', 
+        [opts.text])
+
+      if (row) {
+        this.#lastReply = row
+        return respond(row.reply)
+      }
     }
-  },
+  }
 
-  who: function(respond) {
-    if (module.exports.last) {
-      return respond('This shitpost brought to you by ' + module.exports.last['added_by']);
-    } else {
-      return respond('who said what?');
-    }
-  },
-
-  lastReply: undefined
-
-};
+}
