3f26daa4dd6f9f99124c6d5f4d62f76ed1678696
strong-code <colin@strongco.de>
2021-07-09 01:32:24 -0700

    Update tell command. Move tell observer to es6 classes

--- commands/tell.js
+++ commands/tell.js
@@ -1,6 +1,6 @@
 const db = require('../util/db.js')
 const Command = require('./command.js')
-// const TellObserver = require('../observers/tell.js')
+const TellObserver = require('../observers/tell.js')
 
 module.exports = class Tell extends Command {
 
@@ -19,21 +19,19 @@ module.exports = class Tell extends Command {
     }
   }
 
-  saveMessage(chan, sender, receiver, msg, respond) {
-    // this.toCache(chan, sender, receiver, msg)
-    this.toDisk(chan, sender, receiver, msg)
+  async saveMessage(chan, sender, receiver, msg, respond) {
+    await this.toDisk(chan, sender, receiver, msg)
+    TellObserver.refresh()
     return respond(`Message saved. I will tell ${receiver} next time they are around.`)
   }
 
-  toCache(chan, sender, receiver, msg) {
-    TellObserver.addTell(receiver, { chan: chan, sender: sender, msg: msg })
-  }
-
-  toDisk(chan, sender, receiver, msg) {
-    db.none(
+  async toDisk(chan, sender, receiver, msg) {
+    await db.none(
       'INSERT INTO tells (chan, sender, receiver, message, created_at) VALUES ($1, $2, $3, $4, $5)',
       [chan, sender, receiver, msg, new Date().toISOString()]
-    ).then(() => console.log('Tell message saved to disk'))
+    )
+
+    console.log('Tell message saved to disk')
   }
 
 }
