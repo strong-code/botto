89e66cf16dc7e89dd1f1ff13958c1f10d6c1b463
strong-code <colin@strongco.de>
2021-07-23 13:58:08 -0700

    parsers/steam: basic steam store page parsing. Closes #15

---
+++ observers/parsers/steam.js
@@ -0,0 +1,32 @@
+const needle = require('needle')
+const Helpers = require('../../util/helpers.js')
+
+module.exports = {
+
+  hostMatch: /^(www\.)?(store\.)?steampowered\.com$/,
+
+  parse: async function(url) {
+    const appId = url.path.split('/')[2]
+    const res = await needle('get', `https://store.steampowered.com/api/appdetails?appids=${appId}`)
+    const game = res.body[appId]
+
+    console.log(game)
+    const name = game.data.name
+    const desc = game.data.short_description
+    const site = game.data.website
+    const genres = game.data.genres.map(g => g.description).join(', ')
+    let price
+    let release
+
+    if (game.data.release_date.coming_soon) {
+      price = ''
+      release = `Comes out on ${game.data.release_date.date}`
+    } else {
+      price = game.data.price_overview.final_formatted
+      release = game.data.release_date.date
+    }
+
+    return `[Steam] ${name}: ${desc}. (${genres}) ${release} ${price}`
+  }
+
+}
