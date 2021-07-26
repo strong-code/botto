dd47dec99478aa715c35e406e0b2d2f642abbeb1
strong-code <colin@strongco.de>
2021-07-26 14:53:15 -0700

    parsers/reddit: allow parsing of non-comment reddit pages

--- observers/parsers/reddit.js
+++ observers/parsers/reddit.js
@@ -1,6 +1,7 @@
 const snoowrap = require('snoowrap')
 const config = require('../../config.js').url.reddit
 const r = new snoowrap(config)
+const needle = require('needle')
 
 module.exports = {
 
@@ -9,17 +10,30 @@ module.exports = {
   parse: async function(url) {
     const parts = url.path.split('/')
 
-    // it's not a reddit thread, use general parser
-    if (parts[3] != 'comments') {
-      throw Error('Not a reddit thread, using default parser') 
+    if (parts[3] === 'comments') {
+      return module.exports.parseThread(parts[4])
+    } else if (parts[1] === 'r') {
+      return module.exports.parseSubreddit(parts[2])
+    } else {
+      throw Error('Not a parseable reddit thread, using default parser') 
     }
 
-    let thread = url.path.split('/')[4]
+  },
+
+  parseThread: async function(thread) {
     const t = await r.getSubmission(thread).fetch()
     const date = new Date(t.created*1000).toLocaleString().split(' ')[0].slice(0,-1)
 
     return `[Reddit] ${t.subreddit_name_prefixed}: "${t.title}" posted by u/${t.author.name} on ${date} `+
       `| ${t.comments.length} comments | ${t.ups}↑ - ${t.downs}↓` 
+  },
+
+  parseSubreddit: async function(subreddit) {
+    const res = await needle('get', `https://old.reddit.com/r/${subreddit}/about.json`)
+    const sub = res.body.data
+
+    return `[Reddit] ${sub.url}: ${sub.public_description} ` + 
+    `(${sub.subscribers.toLocaleString()} subscribers, ${sub.active_user_count.toLocaleString()} active users)`
   }
 
 }
