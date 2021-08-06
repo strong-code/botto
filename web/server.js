e0217ed09f53df6ef25b8b9f732ebb8f982534ce
strong-code <colin@strongco.de>
2021-08-05 21:51:24 -0700

    web: set up basic eta templating

--- web/server.js
+++ web/server.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const db = require('../util/db.js') 
+const eta = require('eta')
 const app = express()
 
+app.engine('eta', eta.renderFile)
+app.set('view engine', 'eta')
+app.set('views', 'web/views')
+
+app.get('/healthcheck', (req, res) => {
+  res.status(200).send('OK')
+})
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index', {
+    name: 'eta'
+  })
 })
 
 app.listen(8181, () => {
