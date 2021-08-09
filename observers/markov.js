23fa87788649605c53596790feb0a386265e9b77
strong-code <colin@strongco.de>
2021-08-09 16:28:16 -0700

    markov: add gpt-2 based markov creator

---
+++ observers/markov.js
@@ -0,0 +1,32 @@
+const Observer = require('./observer.js')
+const needle = require('needle')
+const Helpers = require('../util/helpers.js')
+
+module.exports = class Markov extends Observer {
+
+  constructor() {
+    const regex = new RegExp(/^.*botto.*$/i)
+    super('markov', regex)
+  }
+  
+  async call(opts, respond) {
+    const url = 'https://bellard.org/textsynth/api/v1/engines/gptj_6B/completions'
+    const data = {
+      prompt: opts.text.replace('botto', ''),
+      seed: 0,
+      stream: false,
+      temperature: 1,
+      top_k: 40,
+      top_p: 0.8
+    }
+
+    const res = await needle('post', url, data, { json: true })
+    const text = Helpers.strip(res.body.text)
+
+    const limit = Math.ceil(Math.random() * 3) // 1 - 3 sentence limit
+
+    return respond(text.split('.').slice(0, limit).join('.'))
+  }
+
+}
+
