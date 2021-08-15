506711aab0ea9602de1b65eb371b964e458ba022
strong-code <colin@strongco.de>
2021-08-15 16:24:51 -0700

    gas: use etherscan api for average and fast gas pricing. It's more up to date

--- commands/gas.js
+++ commands/gas.js
@@ -1,5 +1,5 @@
 const needle   = require('needle')
-const API_URL  = 'https://ethgasstation.info/json/ethgasAPI.json' 
+const API_URL = `https://api.etherscan.io/api?module=gastracker&action=gasoracle`
 const Command  = require('./command.js')
 
 module.exports = class Gas extends Command {
@@ -16,11 +16,11 @@ module.exports = class Gas extends Command {
       if (res.statusCode != 200)
         return respond(`[Error ${body.status.error_code}] ${body.status.error_message}`)
 
-      const avgGasInGwei = body.average / 10
-      const blockTime = parseInt(body.block_time)
-      const block = body.blockNum.toLocaleString()
+      const block = parseInt(body.result.LastBlock).toLocaleString()
+      const avg = body.result.SafeGasPrice
+      const fast = body.result.FastGasPrice
 
-      respond(`Average gas price is ${avgGasInGwei} gwei. Current block time is ${blockTime} seconds. Current block #${block}.`)
+      respond(`Average gas price is ${avg} gwei. Fast gas price is ${fast} gwei. Current block is #${block}`)
     })
   }
 
