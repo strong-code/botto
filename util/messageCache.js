a7c430692469118be980d4f4df5113a1d44cbf85
strong-code <colin@strongco.de>
2021-09-26 00:07:11 -0700

    messageCache: rename and add simple get()

--- util/messageCache.js
+++ util/messageCache.js
@@ -1,21 +1,25 @@
-const MSG_CACHE_LENGTH = 100
+const MAX_MSG_CACHE_LENGTH = 100
 
 module.exports = class MessageCache {
 
   static #MSG_CACHE = {}
 
-  static cache(from, to, text, time) {
+  static put(from, to, text, time) {
     const msg = {from: from, text: text, time: time}
 
     if (!this.#MSG_CACHE[to]) {
       this.#MSG_CACHE[to] = []
     }
 
-    if (this.#MSG_CACHE[to].length >= MSG_CACHE_LENGTH) {
+    if (this.#MSG_CACHE[to].length >= MAX_MSG_CACHE_LENGTH) {
       this.#MSG_CACHE[to].shift()
     }
 
     this.#MSG_CACHE[to].push(msg)
+  }
+
+  static get(to) {
+    return this.#MSG_CACHE[to]
   }
 
 }
