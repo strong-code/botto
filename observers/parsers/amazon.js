e99a4342d0a851f9bce38a9998c203f217dbfb14
strong-code <colin@strongco.de>
2021-12-17 23:19:48 -0800

    parser/amazon: Refactor to better handle 429 rate limiting

--- observers/parsers/amazon.js
+++ observers/parsers/amazon.js
@@ -17,6 +17,19 @@ module.exports = {
       throw Error('Unable to extract ASIN from ' + url.href)
     }
 
+    let info = await module.exports.getBaseInfo(asin)
+
+    try {
+      const ratings = await module.exports.getRatingInfo(asin)
+      info += ratings
+    } catch (e) {
+      console.log(`Unable to fetch stars/rating info. Continuing without ratings`)
+    }
+
+    return info
+  },
+
+  getBaseInfo: async function(asin) {
     const requestOpts = { 
       'ItemIds': [asin], 
       'Resources': [
@@ -26,31 +39,32 @@ module.exports = {
       ]
     }
 
-    const data = await client.GetItems(config.amazon, requestOpts)
-    const item = data.ItemsResult.Items[0]
-    const price = item.Offers.Listings[0].Price.Amount
-    const desc = _.truncate(item.ItemInfo.Title.DisplayValue, {length: 120})
-        
-    let info = `[${Colors.wrap('orange', 'Amazon')}] $${price} | `
-
     try {
-      const res = await needle('get', ratingsUrl + asin, config.options)
-      const $ = cheerio.load(res.body)
-      const stars = $('.a-icon-alt').text().split(' ')[0]
-      const ratings = $('.totalRatingCount').text().split(' ')[0]
-
-      if (stars === '' || ratings === '') {
-        throw new Error('Triggered bot detection on ratings URL, ignoring')
+      const data = await client.GetItems(config.amazon, requestOpts)
+      const item = data.ItemsResult.Items[0]
+      const price = item.Offers.Listings[0].Price.Amount
+      const desc = _.truncate(item.ItemInfo.Title.DisplayValue, {length: 120})
+    } catch (e) {
+      if (e.status === 429) {
+        const error = JSON.parse(e.response.error.text).Errors[0]
+        throw new Error(`[429] ${error.Message}`)
       }
+    }
+        
+    return `[${Colors.wrap('orange', 'Amazon')}] $${price} | `
+  },
 
-      info += `${Colors.wrap('yellow', `★ ${stars}`)} (${ratings} ratings) | `
-    } catch (error) {
-      console.log(`Unable to fetch ratings from ratingsUrl\n  ${error.message}`)
+  getRatingInfo: async function(asin) {
+    const res = await needle('get', ratingsUrl + asin, config.options)
+    const $ = cheerio.load(res.body)
+    const stars = $('.a-icon-alt').text().split(' ')[0]
+    const ratings = $('.totalRatingCount').text().split(' ')[0]
+
+    if (stars === '' || ratings === '') {
+      throw new Error('Triggered bot detection on ratings URL, ignoring')
     }
 
-    info += desc
-
-    return info
+    return `${Colors.wrap('yellow', `★ ${stars}`)} (${ratings} ratings) | `
   },
 
   asinRegex: /(?:dp|o|gp|product|-)\/(B[0-9]{2}[0-9A-Z]{7}|[0-9]{9}(?:X|[0-9]))/ 
