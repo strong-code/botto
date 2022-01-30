2ffb78471dbdbb9b3025395b599badddaf805722
strong-code <colin@strongco.de>
2022-01-29 21:41:02 -0800

    parsers/reddit: add ability to parse specifically linked comments. Refactor shared code. Closes #28

--- observers/parsers/reddit.js
+++ observers/parsers/reddit.js
@@ -12,6 +12,9 @@ module.exports = {
     const parts = url.path.split('/')
 
     if (parts[3] === 'comments') {
+      if (/^[a-z0-9]{7}$/.test(parts[6])) {
+        return module.exports.parseComment(parts[6])
+      }
       return module.exports.parseThread(parts[4])
     } else if (parts[1] === 'r') {
       return module.exports.parseSubreddit(parts[2])
@@ -21,14 +24,20 @@ module.exports = {
 
   },
 
+  parseComment: async function(comment) {
+    const c = await r.getComment(comment).fetch()
+    const [date, ups, downs] = module.exports.parsePostInfo(c)
+    
+    return `[${Colors.wrap('orange', 'Reddit')}] u/${c.author_fullname}: "${c.body.substr(0,200)}..."`
+      + ` | ${date} | ${ups} ${downs}`
+  },
+
   parseThread: async function(thread) {
     const t = await r.getSubmission(thread).fetch()
-    const date = new Date(t.created*1000).toLocaleString().split(' ')[0].slice(0,-1)
-    const formattedUps = Colors.wrap('light_green', `${t.ups.toLocaleString()} ↑`)
-    const formattedDowns = Colors.wrap('light_red', `${t.downs.toLocaleString()} ↓`)
+    const [date, ups, downs] = module.exports.parsePostInfo(t)
 
     return `[${Colors.wrap('orange', 'Reddit')}] ${t.subreddit_name_prefixed}: "${t.title}" posted by u/${t.author.name}`
-      +` on ${date} | ${t.comments.length.toLocaleString()} comments | ${formattedUps} ${formattedDowns}`
+      +` on ${date} | ${t.comments.length.toLocaleString()} comments | ${ups} ${downs}`
   },
 
   parseSubreddit: async function(subreddit) {
@@ -37,6 +46,14 @@ module.exports = {
 
     return `[Reddit] ${sub.url}: ${sub.public_description} ` + 
     `(${sub.subscribers.toLocaleString()} subscribers, ${sub.active_user_count.toLocaleString()} active users)`
+  },
+
+  parsePostInfo: function(p) {
+    const date = new Date(p.created*1000).toLocaleString().split(' ')[0].slice(0, -1)
+    const ups = Colors.wrap('light_green', `${p.ups.toLocaleString()} ↑`)
+    const downs = Colors.wrap('light_red', `${p.downs.toLocaleString()} ↓`)
+
+    return [date, ups, downs]
   }
 
 }
